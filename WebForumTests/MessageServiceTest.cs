32624b89dc09c15172e8e8195579f1833d79616b
Yana Lazorenko <deccembergirl@gmail.com>
2021-09-27 17:40:24 +0300

    Original version cloned.

---
+++ WebForumTests/MessageServiceTest.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using WebForum;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+
+namespace WebForumTests
+{
+    [TestClass]
+    public class MessageServiceTest
+    {
+        private List<Topic> topicsEntity;
+        private List<TopicControl> topicsDTO;
+        private List<Message> messagesEntity;
+        private List<MessageControl> messagesDTO;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            topicsEntity = new List<Topic> { new Topic { Id = 1, Date = "2010/10/10", Name = "Test Topic Name", Text = "Test First Message" },
+             new Topic { Id = 2, Date = "2013/09/11", Name = "Second Test Topic Name", Text = "Second Test First Message"} };
+            topicsDTO = new List<TopicControl> { new TopicControl { Id = 1, Date = "2010/10/10" , Name = "Test Topic Name", Text = "Test First Message" },
+            new TopicControl {Id = 2, Date = "2013/09/11", Name = "Second Test Topic Name", Text = "Second Test First Message"} };
+            messagesEntity = new List<Message> { new Message { Id = 1, Date = "2013/09/09", Text = "First message!" },
+            new Message{Id = 2, Date = "2012/07/09", Text = "Second message!"} };
+            messagesDTO = new List<MessageControl> { new MessageControl { Id = 1, Date = "2013/09/09", Text = "First message!", TopicId = 1 },
+            new MessageControl{Id = 2, Date = "2012/07/09", Text = "Second message!", TopicId=2}};
+        }
+        [TestMethod]
+        public void Given_Valid_Id_When_DeleteByIdAsync_Message_Invokes_Delete_From_Repository_Once()
+        {
+            //Arrange
+            var mapper = new Mock<IMapper>();
+            var unitOfWork = new Mock<UnitOfWork>();
+            var repository = new Mock<MessageRepository>();
+            var messageService = new MessageService(unitOfWork.Object, mapper.Object);
+            unitOfWork.Setup(x => x.MessageRepository.DeleteByIdAsync(1)).Verifiable();
+            //Act
+            messageService.DeleteByIdAsync(1);
+            //Assert
+            unitOfWork.Verify(x => x.MessageRepository.DeleteByIdAsync(1), Times.Once);
+        }
+        [TestMethod]
+        public void Given_Valid_Id_When_Update_Message_Invokes_Delete_From_Repository_Once()
+        {
+            //Arrange
+            var mapper = new Mock<IMapper>();
+            var unitOfWork = new Mock<UnitOfWork>();
+            var repository = new Mock<MessageRepository>();
+            var messageService = new MessageService(unitOfWork.Object, mapper.Object);
+            unitOfWork.Setup(x => x.MessageRepository.Update(messagesEntity[1])).Verifiable();
+            mapper.Setup(x => x.Map<MessageControl, Message>(messagesDTO[1])).Returns(messagesEntity[1]);
+            //Act
+            messageService.UpdateAsync(messagesDTO[1]).Wait();
+            //Assert
+            unitOfWork.Verify(x => x.MessageRepository.Update(messagesEntity[1]), Times.Once);
+        }
+    }
+ }
---
+++ WebForumTests/TopicServiceTest.cs
@@ -0,0 +1,110 @@
+﻿using AutoMapper;
+using WebForum;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebForumTests
+{
+    [TestClass]
+    public class TopicServiceTest
+    {
+        private List<Topic> topicsEntity;
+        private List<TopicControl> topicsDTO;
+        
+
+        [TestInitialize]
+        public void Setup()
+        {
+            topicsEntity = new List<Topic> { new Topic { Id = 1, Date = "2010/10/10", Name = "Test Topic Name", Text = "Test First Message" },
+             new Topic { Id = 2, Date = "2013/09/11", Name = "Second Test Topic Name", Text = "Second Test First Message"} };
+            topicsDTO = new List<TopicControl> { new TopicControl { Id = 1, Date = "2010/10/10" , Name = "Test Topic Name", Text = "Test First Message" },
+            new TopicControl {Id = 2, Date = "2013/09/11", Name = "Second Test Topic Name", Text = "Second Test First Message"} };
+            
+        }
+
+        [TestMethod]
+        public void Given_Valid_Entities_When_GetAll_Then_Return_DTOs()
+        {
+            //Arrange
+            var mapper = new Mock<IMapper>();
+            var unitOfWork = new Mock<UnitOfWork>();
+            var messageService = new Mock<MessageService>();
+            var topicService = new TopicService(unitOfWork.Object, mapper.Object, messageService.Object);
+            unitOfWork.Setup(x => x.TopicRepository.FindAll()).Returns(topicsEntity.AsQueryable());
+            foreach (var topic in topicsEntity)
+            {
+                mapper.Setup(x => x.Map<Topic,TopicControl>(topic)).Returns(topicsDTO.First(x=>x.Id==topic.Id));
+            }
+            //Act
+            var allTopicsResult = topicService.GetAll();
+            //Assert
+            CollectionAssert.AreEquivalent(topicsDTO, allTopicsResult.ToList());
+        }
+        [TestMethod]
+        public void Given_Null_Entities_When_GetAll_Then_Return_Null()
+        {
+            //Arrange
+            var mapper = new Mock<IMapper>();
+            var unitOfWork = new Mock<UnitOfWork>();
+            var messageService = new Mock<MessageService>();
+            var topicService = new TopicService(unitOfWork.Object, mapper.Object, messageService.Object);
+            unitOfWork.Setup(x => x.TopicRepository.FindAll()).Returns((IQueryable<Topic>)null);
+            //Act
+            var allTopicsResult = topicService.GetAll();
+            //Assert
+            Assert.IsNull(allTopicsResult);
+        }
+
+
+        [TestMethod]
+        public void Given_Valid_Entities_When_GetByIdAsync_Then_Return_DTO()
+        {
+            //Arrange
+            var mapper = new Mock<IMapper>();
+            var unitOfWork = new Mock<UnitOfWork>();
+            var messageService = new Mock<MessageService>();
+            var topicService = new TopicService(unitOfWork.Object, mapper.Object, messageService.Object);
+            unitOfWork.Setup(x => x.TopicRepository.GetByIdAsync(It.IsAny<int>())).Returns(Task.FromResult(topicsEntity[1]));
+            mapper.Setup(x => x.Map<Topic, TopicControl>(topicsEntity[1])).Returns(topicsDTO[1]);
+         
+            //Act
+            var TopicResult = topicService.GetById(1,1).Result;
+            //Assert
+            Assert.AreEqual(topicsDTO[1], TopicResult);
+        }
+       
+        [TestMethod]
+        public void Given_Valid_Id_When_DeleteByIdAsync_Invokes_Delete_From_Repository_Once()
+        {
+            //Arrange
+            var mapper = new Mock<IMapper>();
+            var unitOfWork = new Mock<UnitOfWork>();
+            var repository = new Mock<TopicRepository>();
+            var messageService = new Mock<MessageService>();
+            var topicService = new TopicService(unitOfWork.Object, mapper.Object, messageService.Object);
+            unitOfWork.Setup(x => x.TopicRepository.DeleteByIdAsync(1)).Verifiable();
+            //Act
+            topicService.DeleteByIdAsync(1);
+            //Assert
+            unitOfWork.Verify(x => x.TopicRepository.DeleteByIdAsync(1), Times.Once);
+        }
+        [TestMethod]
+        public void Given_Valid_Entity_When_UpdateAsync_From_Repository_Once()
+        {
+            //Arrange
+            var mapper = new Mock<IMapper>();
+            var unitOfWork = new Mock<UnitOfWork>();
+            var repository = new Mock<TopicRepository>();
+            var messageService = new Mock<MessageService>();
+            var topicService = new TopicService(unitOfWork.Object, mapper.Object, messageService.Object);
+            unitOfWork.Setup(x => x.TopicRepository.Update(topicsEntity[1])).Verifiable();
+            mapper.Setup(x => x.Map<TopicControl,Topic>(topicsDTO[1])).Returns(topicsEntity[1]);
+            //Act
+            topicService.UpdateAsync(topicsDTO[1]).Wait();
+            //Assert
+            unitOfWork.Verify(x => x.TopicRepository.Update(topicsEntity[1]), Times.Once);
+        }
+    }
+}
