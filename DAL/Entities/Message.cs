d0268823670bbeee28da3d13f64bc7c9356f6dfc
Yana Lazorenko <deccembergirl@gmail.com>
2021-10-22 13:21:26 +0300

    Entities are separated to DAL.

---
+++ DAL/Entities/Message.cs
@@ -0,0 +1,28 @@
+﻿namespace DAL.Entities
+{
+    /// <summary>
+    /// Message entity
+    /// </summary>
+    public class Message : BaseEntity
+    {
+        /// <summary>
+        /// The date of the message creating
+        /// </summary>
+        public string Date { get; set; }
+
+        /// <summary>
+        /// Text of the message
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Topic which the message corresponds to
+        /// </summary>
+        public virtual Topic Topic { get; set; }
+
+        /// <summary>
+        /// The author of the message
+        /// </summary>
+        public virtual ForumUser ForumUser { get; set; }
+    }
+}
---
+++ DAL/Entities/Topic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Entities
+{
+    /// <summary>
+    /// Topic entity
+    /// </summary>
+    public class Topic : BaseEntity
+    {
+        /// <summary>
+        /// Topic Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Topic name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The date of the topic creating
+        /// </summary>
+        public string Date { get; set; }
+
+        /// <summary>
+        /// Description of the topic
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// The topic creator
+        /// </summary>
+        public virtual ForumUser ForumUser { get; set; }
+
+        /// <summary>
+        /// The collection of messages which corresponds to the topic
+        /// </summary>
+        public virtual ICollection<Message> Messages { get; set; }
+    }
+}
