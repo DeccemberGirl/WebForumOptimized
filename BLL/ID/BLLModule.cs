dea911b1744ebfc6122bd8b70559ed0463552267
Nensy <brilliantova1113@s18.pp.ua>
2021-10-26 22:32:05 -0500

    Dependencies injected implemented

---
+++ BLL/ID/BLLModule.cs
@@ -0,0 +1,16 @@
+﻿using DAL;
+using Ninject.Modules;
+
+namespace BLL.DI
+{
+    /// <summary>
+    /// Ninject module for registering needed dependencies for BLL
+    /// </summary>
+    public class BLLModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<UnitOfWork>();
+        }
+    }
+}
