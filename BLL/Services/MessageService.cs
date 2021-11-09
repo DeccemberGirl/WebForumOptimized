8a48faec36683ff65ca16ce1bfd8b50a3d090192
Nensy <brilliantova1113@s18.pp.ua>
2021-11-09 19:22:22 +0200

    Added message service to BLL

--- BLL/Services/MessageService.cs
+++ BLL/Services/MessageService.cs
@@ -1,49 +1,56 @@
 ﻿using AutoMapper;
+using BLL.Builders.Interfaces;
+using BLL.DTO;
+using BLL.Models;
+using BLL.Services.Interfaces;
+using DAL;
+using DAL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebForum
+namespace BLL.Services
 {
     /// <summary>
     /// Service for Messages
     /// </summary>
-    public class MessageService
+    public class MessageService : IMessageService
     {
-        private readonly UnitOfWork _unitOfWork;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-        public MessageService(UnitOfWork db, IMapper mapper)
+        private readonly IMessageDTOBuilder _messageDTOBuilder;
+        public MessageService(IUnitOfWork db, IMapper mapper, IMessageDTOBuilder messageDTOBuilder)
         {
             _mapper = mapper;
             _unitOfWork = db;
+            _messageDTOBuilder = messageDTOBuilder;
         }
-        public IEnumerable<MessageControl> GetAll()
+        /// <summary>
+        /// Allows to get all messages
+        /// </summary>
+        public IEnumerable<MessageDTO> GetAll()
         {
             var entities = _unitOfWork.MessageRepository.FindAll();
-            var result = _mapper.MapList<Message, MessageControl>(entities);
+            var result = _mapper.MapList<Message, MessageDTO>(entities);
             return result;
         }
-        public PagedMessagesModel GetPagedMessages(IEnumerable<MessageControl> messages, int pageNumber)
+        /// <summary>
+        /// Allows to get all messages on page
+        /// </summary>
+        public PagedMessagesModel GetPagedMessages(IEnumerable<MessageDTO> messages, int pageNumber)
         {
             var numberOfMessages = messages.Count();
-            var numberOfPages = (int)Math.Ceiling((double)numberOfMessages / 6);
+            var numberOfPages = (int)Math.Ceiling((double)numberOfMessages / (double)Constants.PageSize);
             if (pageNumber > numberOfPages)
             {
                 pageNumber = 1;
             }
             var totalPages = numberOfPages;
-            var messagesResult = messages.OrderBy(x => x.Id).Skip((pageNumber - 1) * 6).Take(6);
+            var messagesResult = messages.OrderBy(x => x.Id).Skip((pageNumber - 1) * Constants.PageSize).Take(Constants.PageSize);
             var pageModel = new PagedMessagesModel() { CurrentPage = pageNumber, Messages = messagesResult, TotalPages = totalPages };
             return pageModel;
         }
-
-        public MessageControl Create(string userId, string userName, string text)
-        {
-            var messageDTO = new MessageControl { Date = DateTime.Now.ToString(), UserForumId = userId, UserName = userName, Text = text };
-            return messageDTO;
-        }
-
         /// <summary>
         /// Adds new message to topic
         /// </summary>
@@ -56,10 +63,10 @@ namespace WebForum
         {
             if (String.IsNullOrEmpty(userId) || String.IsNullOrEmpty(userName) || String.IsNullOrEmpty(text) || topicId == 0)
                 return;
-            var inputMessageDTO = Create(userId, userName, text);
-            var message = _mapper.Map<MessageControl, Message>(inputMessageDTO);
+            var inputMessageDTO = _messageDTOBuilder.Create(userId, userName, text);
+            var message = _mapper.Map<MessageDTO, Message>(inputMessageDTO);
             message.ForumUser = await _unitOfWork.UserManager.FindByIdAsync(userId);
-            await _unitOfWork.MessageRepository.AddAsyncMessageToTopic(message,topicId);
+            await _unitOfWork.MessageRepository.AddAsyncMessageToTopic(message, topicId);
             await _unitOfWork.SaveAsync();
         }
         /// <summary>
@@ -79,11 +86,11 @@ namespace WebForum
         /// </summary>
         /// <param name="message"></param>
         /// <returns></returns>
-        public async Task UpdateAsync(MessageControl message)
+        public async Task UpdateAsync(MessageDTO message)
         {
             if (message == null)
                 return;
-            await _unitOfWork.MessageRepository.Update(_mapper.Map<MessageControl, Message>(message));
+            await _unitOfWork.MessageRepository.Update(_mapper.Map<MessageDTO, Message>(message));
             await _unitOfWork.SaveAsync();
         }
     }
---
+++ DAL/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace DAL
+{
+    /// <summary>
+    /// Constants list
+    /// </summary>
+    public static class Constants
+    {
+        public const int PageSize = 6;
+    }
+}
